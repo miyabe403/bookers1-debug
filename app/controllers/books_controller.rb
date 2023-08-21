954a969ec7041d61549222c21ab3caa8ab7f9c3c
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-21 13:10:10 +0900

    [Fix] 投稿の編集が行えないので「何をどう更新するか」を指定するために、 @book.update(...)と、後ろに引数を設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params) # 後ろに引数を
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
